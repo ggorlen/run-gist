79d4fe088f94e4c04104e61888dec7501ea45179
Greg Gorlen <gsgorlen@gmail.com>
2026-01-31 17:58:05 -0800

    Add support for more langs

--- README.md
+++ README.md
@@ -4,56 +4,64 @@ Run a GitHub gist. This looks for supported code files in the gist and executes 
 
 ### Examples
 
-- HTML file: <https://ggorlen.github.io/run-gist/?id=1234f531e250c4858ee45e650946ab22>
-- Python script: <https://ggorlen.github.io/run-gist/?id=1811236>
-- Python script with deps: <https://ggorlen.github.io/run-gist/?id=35e3f829ded6d826626762da50b7a003>
-- JS script: <https://ggorlen.github.io/run-gist/?id=faf38135846db5a1a93a69875dd0761d>
-- Lua script: <https://ggorlen.github.io/run-gist/?id=b420d1ecc60ad6ec44e5>
+- HTML: <https://ggorlen.github.io/run-gist/?id=1234f531e250c4858ee45e650946ab22>
+- Python: <https://ggorlen.github.io/run-gist/?id=1811236>
+- Python with deps: <https://ggorlen.github.io/run-gist/?id=35e3f829ded6d826626762da50b7a003>
+- JS: <https://ggorlen.github.io/run-gist/?id=faf38135846db5a1a93a69875dd0761d>
+- Lua: <https://ggorlen.github.io/run-gist/?id=b420d1ecc60ad6ec44e5>
+- Perl: <https://ggorlen.github.io/run-gist/?id=5820275>
+- Ruby: <https://ggorlen.github.io/run-gist/?id=d7cab2f5d98307909f3e53cbf61ffdce>
+- PHP: <https://ggorlen.github.io/run-gist/?id=bf0fb63432a90c3de93a427f013bd2af>
+- SQLite: <https://ggorlen.github.io/run-gist/?id=d25dcbe8cd0e6982d3f315dfd298034a&lang=sqlite>
+- Postgres: <https://ggorlen.github.io/run-gist/?id=d25dcbe8cd0e6982d3f315dfd298034a&lang=postgres>
+- Markdown: <https://ggorlen.github.io/run-gist/?id=8eccfe543a74717b13703cf7a2e03679>
+- TypeScript: <https://ggorlen.github.io/run-gist/?id=ea91750c08ee2de80f8a4176bed85623>
+
+Not yet fully working:
+- Text: <https://ggorlen.github.io/run-gist/?id=35e3f829ded6d826626762da50b7a003&file=requirements.txt> (needs to infer lang from extension)
+- R: <https://ggorlen.github.io/run-gist/?id=51f3bc251a47bce643b3f4c069ffccd3> (needs to print to DOM rather than just console)
+- Scheme: <https://ggorlen.github.io/run-gist/?id=1854679> (no output)
+- ClojureScript: <https://ggorlen.github.io/run-gist/?id=1343920> (no output)
+- JSON: <https://ggorlen.github.io/run-gist/?id=5f5f8bd37428502cac6d960fabb30245> (poor color contrast)
+- YAML: <https://ggorlen.github.io/run-gist/?id=1df3e721f732f958688c076674f81aee> (poor color contrast)
 
 ### TODO
 
 - Parse and include CSS/JS files included from HTML
   - HTML/CSS/JS example to make work: <https://ggorlen.github.io/run-gist/?id=eedd8f9f754706347b63d2baa95ca73c>
-  - Render markdown with marked
-  - Render JSON/plain text preformatted
 - Add support for other WASM languages (might have to move away from no-build though)
   - https://meta.stackoverflow.com/a/394310/6243352 (thread has ClojureScript and Fengari)
   - [Run Python turtle in the browser with Skulpt](https://meta.stackoverflow.com/a/438047/6243352)
-  - https://github.com/seanmorris/php-wasm ([works](https://meta.stackoverflow.com/a/438047/6243352), although there's no obvious way to eval PHP in JS, so [this](https://github.com/oraoto/pib/tree/gh-pages) may be needed)
   - https://github.com/r-wasm/webr (works)
     - https://webr.sh/
-  - https://github.com/replit-archive/emscripted-ruby (wasn't able to get working on a quick spike)
-  - sqlite (works)
-  - postgres (works)
+  - shell scripting langs
   - assembly langs (need more attention)
-  - https://github.com/haukex/webperl (works)
-  - TS (works, except no errors)
+    - SPIM I already have https://github.com/ggorlen/spimjs
   - <https://github.com/ocsigen/js_of_ocaml> (haven't tried)
   - https://github.com/Keno/julia-wasm (haven't tried)
-  - https://github.com/Doridian/LuaJS (haven't tried)
   - https://popcorn.swmansion.com (haven't tried)
+  - https://github.com/replit-archive/emscripted-ruby (wasn't able to get working on a quick spike)
+  - https://github.com/Doridian/LuaJS (haven't tried, probably not needed since we're using Fengari)
   - esolangs
   - Resources: [1](https://github.com/mbasso/awesome-wasm), [2](https://github.com/appcypher/awesome-wasm-langs)
-- have an optional query param for which file/lang to run
 - Could have it run files from github repos following similar patterns
 - Maybe add a feature to [list all gists for a user](https://github.com/ggorlen/gist-list) and let them click one to open/exec/edit it. Better: just add a link to this run-gist app into that repo.
-- Add editing/saving options. See [this repo](https://github.com/greggman/jsgist?tab=readme-ov-file), [this project](https://scriptpad.dev/?id=ybqiqNVknx0aYPMCaTlJ&o=1&c=0) and [this repo](https://github.com/gist-run/gist-run) for inspiration.
+- Add editing/saving options. See [this repo](https://github.com/greggman/jsgist), [this project](https://scriptpad.dev/?id=ybqiqNVknx0aYPMCaTlJ&o=1&c=0) and [this repo](https://github.com/gist-run/gist-run) for inspiration.
 - Options to open in new window or replace current, optionally keep run gist header bar
 - Consider escaping or rendering HTML characters from Python scripts
   - Theoretically could follow this further to make web apps with backend languages
 - Load Pyodide packages with [micropip](https://micropip.pyodide.org/en/v0.7.1/project/api.html#micropip.install)
-- Upgrade Lua VM to [Fengari](https://stackoverflow.com/a/79876021/6243352)
-  - `console.log(fengari.load(luaCode)());`
 - Try sandboxed JS execution with [quickjs](https://github.com/justjake/quickjs-emscripten?tab=readme-ov-file#using-in-the-browser-without-a-build-step)
-- Move Pyodide and other large script tags out of the base index file and lazy-load them
-- maybe have option to run gist within the same page, so there's a little banner to run another gist?
 - infinite loop detection?
 - https://news.ycombinator.com/item?id=46762150 browser is the sandbox
+- could run code blocks within markdown file?
 - add a way to execute a gist locally?
 - add e2e tests that run every month to check for regressions, or maybe even an action to find version bumps
 
 ### Similar tools
 
+- https://gist.cafe/
 - https://www.pythonanywhere.com/gists/
-- gist.cafe
-- (someone I follow, maybe potch, has a similar project I need to find here)
+- https://github.com/gist-run/gist-run
+- https://www.jspad.dev/
+- https://github.com/greggman/jsgist
--- index.js
+++ index.js
@@ -45,12 +45,22 @@ const runJavaScript = (content) => {
   el.innerHTML = "<pre></pre>";
   const pre = el.querySelector("pre");
   const originalLog = console.log;
+  const originalError = console.error;
   console.log = (...args) => {
     pre.textContent += `${args.join(" ")}\n`;
     originalLog.apply(console, args);
   };
-  eval(content);
+  console.error = (...args) => {
+    pre.textContent += `${args.join(" ")}\n`;
+    originalError.apply(console, args);
+  };
+  try {
+    eval(content);
+  } catch (err) {
+    console.error(err);
+  }
   console.log = originalLog;
+  console.error = originalError;
 };
 
 const runLua = async (content) => {
@@ -58,16 +68,25 @@ const runLua = async (content) => {
   el.innerHTML = "<pre></pre>";
   const pre = el.querySelector("pre");
   pre.textContent = "Loading Lua...\n";
-  await loadScript("https://cdn.jsdelivr.net/npm/lua.vm.js");
+  await loadScript("https://cdn.jsdelivr.net/npm/fengari-web@0.1.4/dist/fengari-web.js");
   pre.textContent = "";
-  const lua = new Lua.State();
   const originalLog = console.log;
+  const originalError = console.error;
   console.log = (...args) => {
     pre.textContent += `${args.join(" ")}\n`;
     originalLog.apply(console, args);
   };
-  lua.execute(content);
+  console.error = (...args) => {
+    pre.textContent += `${args.join(" ")}\n`;
+    originalError.apply(console, args);
+  };
+  try {
+    fengari.load(content)();
+  } catch (err) {
+    console.error(err);
+  }
   console.log = originalLog;
+  console.error = originalError;
 };
 
 const runPython = async (content, requirements) => {
@@ -121,42 +140,524 @@ const runPython = async (content, requirements) => {
   }
 };
 
+const runPerl = async (content) => {
+  const el = document.querySelector("body");
+  el.innerHTML = "<pre></pre>";
+  const pre = el.querySelector("pre");
+  pre.textContent = "Loading Perl...\n";
+
+  await loadScript("https://webperlcdn.zero-g.net/v0.09-beta/webperl.js");
+
+  Perl.noMountIdbfs = true;
+  Perl.endAfterMain = true;
+  Perl.output = (str) => {
+    if (pre.textContent.startsWith("Loading")) {
+      pre.textContent = "";
+    }
+    console.log(str);
+    pre.textContent += str;
+  };
+
+  Perl.init(() => {
+    try {
+      FS.writeFile("/script.pl", content);
+      Perl.start(["/script.pl"]);
+    } catch (err) {
+      console.error(err);
+      pre.textContent += err.message + "\n";
+    }
+  });
+};
+
+const runRuby = async (content) => {
+  const el = document.querySelector("body");
+  el.innerHTML = "<pre></pre>";
+  const pre = el.querySelector("pre");
+  pre.textContent = "Loading Ruby...\n";
+
+  const codeScript = document.createElement("script");
+  codeScript.type = "text/ruby";
+  codeScript.textContent = content;
+  document.body.insertBefore(codeScript, pre);
+
+  const originalLog = console.log;
+  const originalWarn = console.warn;
+  const originalError = console.error;
+  const clearLoading = () => {
+    if (pre.textContent.startsWith("Loading")) pre.textContent = "";
+  };
+  console.log = (...args) => {
+    clearLoading();
+    pre.textContent += args.join(" ") + "\n";
+    originalLog.apply(console, args);
+  };
+  console.warn = (...args) => {
+    originalWarn.apply(console, args);
+  };
+  console.error = (...args) => {
+    clearLoading();
+    pre.textContent += args.join(" ") + "\n";
+    originalError.apply(console, args);
+  };
+
+  await loadScript(
+    "https://cdn.jsdelivr.net/npm/@ruby/3.4-wasm-wasi@2.7.1/dist/browser.script.iife.js",
+  );
+};
+
+const runPHP = async (content) => {
+  const el = document.querySelector("body");
+  el.innerHTML = "<pre></pre>";
+  const pre = el.querySelector("pre");
+  pre.textContent = "Loading PHP...\n";
+
+  const codeScript = document.createElement("script");
+  codeScript.type = "text/php";
+  codeScript.setAttribute("data-stdout", "pre");
+  codeScript.textContent = content;
+  document.body.insertBefore(codeScript, pre);
+
+  const originalLog = console.log;
+  const originalError = console.error;
+  const clearLoading = () => {
+    if (pre.textContent.startsWith("Loading")) {
+      pre.textContent = "";
+    }
+  };
+  console.log = (...args) => {
+    clearLoading();
+    originalLog.apply(console, args);
+  };
+  console.error = (...args) => {
+    clearLoading();
+    pre.textContent += args.join(" ") + "\n";
+    originalError.apply(console, args);
+  };
+
+  await loadScript("https://cdn.jsdelivr.net/npm/php-wasm@0.0.8/php-tags.jsdelivr.mjs");
+};
+
+const runR = async (content) => {
+  const el = document.querySelector("body");
+  el.innerHTML = "<pre></pre>";
+  const pre = el.querySelector("pre");
+  pre.textContent = "Loading R...\n";
+
+  const { WebR } = await import("https://webr.r-wasm.org/latest/webr.mjs");
+  const webR = new WebR();
+  await webR.init();
+
+  pre.textContent = "";
+
+  const originalWarn = console.warn;
+  console.warn = () => {};
+
+  try {
+    await webR.evalR(content);
+  } catch (err) {
+    console.error(err);
+    pre.textContent += err.message + "\n";
+  }
+
+  console.warn = originalWarn;
+};
+
+const runScheme = async (content) => {
+  const el = document.querySelector("body");
+  el.innerHTML = "<pre id='bs-console'></pre>";
+  const pre = el.querySelector("pre");
+  pre.textContent = "Loading Scheme...\n";
+
+  const codeScript = document.createElement("script");
+  codeScript.type = "text/biwascheme";
+  codeScript.textContent = content;
+  document.body.insertBefore(codeScript, pre);
+
+  const originalLog = console.log;
+  const originalError = console.error;
+  const clearLoading = () => {
+    if (pre.textContent.startsWith("Loading")) pre.textContent = "";
+  };
+  console.log = (...args) => {
+    clearLoading();
+    originalLog.apply(console, args);
+  };
+  console.error = (...args) => {
+    clearLoading();
+    pre.textContent += args.join(" ") + "\n";
+    originalError.apply(console, args);
+  };
+
+  await loadScript("https://cdn.jsdelivr.net/npm/biwascheme@0.8.3/release/biwascheme.js");
+};
+
+const runTypeScript = async (content) => {
+  const el = document.querySelector("body");
+  el.innerHTML = "<pre></pre>";
+  const pre = el.querySelector("pre");
+  pre.textContent = "Loading TypeScript...\n";
+
+  await loadScript("https://cdn.jsdelivr.net/npm/typescript@5.7.3/lib/typescript.js");
+
+  pre.textContent = "";
+
+  const sourceFile = ts.createSourceFile("input.ts", content, ts.ScriptTarget.ES2020, true);
+  const diagnostics = [];
+  const host = {
+    getSourceFile: (name) => (name === "input.ts" ? sourceFile : undefined),
+    writeFile: () => {},
+    getDefaultLibFileName: () => "lib.d.ts",
+    useCaseSensitiveFileNames: () => false,
+    getCanonicalFileName: (f) => f,
+    getCurrentDirectory: () => "",
+    getNewLine: () => "\n",
+    fileExists: (f) => f === "input.ts",
+    readFile: () => "",
+  };
+  const program = ts.createProgram(["input.ts"], { target: ts.ScriptTarget.ES2020 }, host);
+  diagnostics.push(...ts.getPreEmitDiagnostics(program));
+
+  if (diagnostics.length > 0) {
+    const errors = diagnostics.map((d) => {
+      if (d.file && d.start !== undefined) {
+        const { line, character } = d.file.getLineAndCharacterOfPosition(d.start);
+        return `(${line + 1},${character + 1}) ${ts.flattenDiagnosticMessageText(d.messageText, "\n")}`;
+      }
+
+      return ts.flattenDiagnosticMessageText(d.messageText, "\n");
+    });
+    const errText = errors.join("\n");
+    console.error(errText);
+    pre.textContent = errText + "\n\n";
+  }
+
+  const originalLog = console.log;
+  const originalError = console.error;
+  console.log = (...args) => {
+    pre.textContent += args.join(" ") + "\n";
+    originalLog.apply(console, args);
+  };
+  console.error = (...args) => {
+    pre.textContent += args.join(" ") + "\n";
+    originalError.apply(console, args);
+  };
+
+  try {
+    const js = ts.transpileModule(content, {
+      compilerOptions: { target: ts.ScriptTarget.ES2020 },
+    }).outputText;
+    eval(js);
+  } catch (err) {
+    console.error(err);
+  }
+
+  console.log = originalLog;
+  console.error = originalError;
+};
+
+const runSQLite = async (content) => {
+  const el = document.querySelector("body");
+  el.innerHTML = "<pre></pre>";
+  const pre = el.querySelector("pre");
+  pre.textContent = "Loading SQLite...\n";
+
+  await loadScript("https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.10.2/sql-wasm.js");
+
+  const SQL = await initSqlJs({
+    locateFile: (file) =>
+      `https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.10.2/${file}`,
+  });
+  const db = new SQL.Database();
+
+  pre.textContent = "";
+
+  try {
+    const result = db.exec(content);
+    const output = JSON.stringify(result, null, 2);
+    console.log(result);
+    pre.textContent = output;
+  } catch (err) {
+    console.error(err);
+    pre.textContent = err.message;
+  }
+};
+
+const runPostgres = async (content) => {
+  const el = document.querySelector("body");
+  el.innerHTML = "<pre></pre>";
+  const pre = el.querySelector("pre");
+  pre.textContent = "Loading Postgres...\n";
+
+  const { PGlite } = await import(
+    "https://cdn.jsdelivr.net/npm/@electric-sql/pglite@0.2.14/dist/index.js"
+  );
+  const db = new PGlite();
+
+  pre.textContent = "";
+
+  try {
+    const result = await db.exec(content);
+    const output = JSON.stringify(result, null, 2);
+    console.log(result);
+    pre.textContent = output;
+  } catch (err) {
+    console.error(err);
+    pre.textContent = err.message;
+  }
+};
+
+const renderDataWithJSON = async (data) => {
+  const el = document.querySelector("body");
+  el.innerHTML = "<div id='json-output'></div>";
+  const container = el.querySelector("#json-output");
+
+  await loadScript("https://cdn.jsdelivr.net/npm/renderjson@1.4.0/renderjson.min.js");
+
+  renderjson.set_show_to_level(2);
+  renderjson.set_icons("+", "-");
+
+  console.log(data);
+  container.appendChild(renderjson(data));
+
+  if (!document.querySelector("#renderjson-styles")) {
+    const style = document.createElement("style");
+    style.id = "renderjson-styles";
+    style.textContent = `
+      #json-output { font-family: monospace; font-size: 14px; }
+      .renderjson a { text-decoration: none; cursor: pointer; }
+      .renderjson .disclosure { color: #888; margin-right: 4px; }
+      .renderjson .string { color: #a31515; }
+      .renderjson .number { color: #098658; }
+      .renderjson .boolean { color: #0000ff; }
+      .renderjson .key { color: #001080; }
+      .renderjson .keyword { color: #0000ff; }
+    `;
+    document.head.appendChild(style);
+  }
+};
+
+const runJSON = async (content) => {
+  try {
+    await renderDataWithJSON(JSON.parse(content));
+  } catch (err) {
+    console.error(err);
+    document.body.innerHTML = `<pre>${err.message}</pre>`;
+  }
+};
+
+const runText = (content) => {
+  const el = document.querySelector("body");
+  el.innerHTML = "<pre></pre>";
+  const pre = el.querySelector("pre");
+  console.log(content);
+  pre.textContent = content;
+};
+
+const runClojureScript = async (content) => {
+  const el = document.querySelector("body");
+  el.innerHTML = "<pre></pre>";
+  const pre = el.querySelector("pre");
+  pre.textContent = "Loading ClojureScript...\n";
+
+  const codeScript = document.createElement("script");
+  codeScript.type = "application/x-scittle";
+  codeScript.textContent = content;
+  document.body.insertBefore(codeScript, pre);
+
+  const originalLog = console.log;
+  const originalError = console.error;
+  const clearLoading = () => {
+    if (pre.textContent.startsWith("Loading ClojureScript...")) {
+      pre.textContent = "";
+    }
+  };
+  console.log = (...args) => {
+    clearLoading();
+    pre.textContent += args.join(" ") + "\n";
+    originalLog.apply(console, args);
+  };
+  console.error = (...args) => {
+    clearLoading();
+    pre.textContent += args.join(" ") + "\n";
+    originalError.apply(console, args);
+  };
+
+  await loadScript("https://cdn.jsdelivr.net/npm/scittle@0.6.22/dist/scittle.js");
+};
+
+const runYAML = async (content) => {
+  try {
+    await loadScript("https://cdn.jsdelivr.net/npm/js-yaml@4.1.0/dist/js-yaml.min.js");
+    await renderDataWithJSON(jsyaml.load(content));
+  } catch (err) {
+    console.error(err);
+    document.body.innerHTML = `<pre>${err.message}</pre>`;
+  }
+};
+
+const runMarkdown = async (content) => {
+  const el = document.querySelector("body");
+  el.innerHTML = "<article></article>";
+  const article = el.querySelector("article");
+
+  await loadScript("https://cdn.jsdelivr.net/npm/marked@12.0.0/marked.min.js");
+
+  try {
+    const html = marked.parse(content);
+    console.log(content);
+    article.innerHTML = html;
+  } catch (err) {
+    console.error(err);
+    article.innerHTML = `<pre>${err.message}</pre>`;
+  }
+
+  const style = document.createElement("style");
+  style.textContent = `
+    article { max-width: 800px; margin: 0 auto; padding: 20px; font-family: system-ui, sans-serif; line-height: 1.6; }
+    article pre { background: #f4f4f4; padding: 12px; overflow-x: auto; border-radius: 4px; }
+    article code { background: #f4f4f4; padding: 2px 6px; border-radius: 3px; }
+    article pre code { background: none; padding: 0; }
+    article blockquote { border-left: 4px solid #ddd; margin-left: 0; padding-left: 16px; color: #666; }
+    article table { border-collapse: collapse; width: 100%; }
+    article th, article td { border: 1px solid #ddd; padding: 8px; text-align: left; }
+    article th { background: #f4f4f4; }
+  `;
+  document.head.appendChild(style);
+};
+
 const languageRunners = {
   HTML: runHTML,
   JavaScript: runJavaScript,
   Lua: runLua,
   Python: runPython,
+  Perl: runPerl,
+  Ruby: runRuby,
+  PHP: runPHP,
+  R: runR,
+  Scheme: runScheme,
+  TypeScript: runTypeScript,
+  SQL: runSQLite,
+  SQLite: runSQLite,
+  PLpgSQL: runPostgres,
+  Postgres: runPostgres,
+  Clojure: runClojureScript,
+  ClojureScript: runClojureScript,
+  JSON: runJSON,
+  YAML: runYAML,
+  Text: runText,
+  Markdown: runMarkdown,
 };
 
-const runGist = async (gistId) => {
+const languagePriority = [
+  "Python",
+  "TypeScript",
+  "JavaScript",
+  "Ruby",
+  "Perl",
+  "PHP",
+  "Lua",
+  "R",
+  "Scheme",
+  "Clojure",
+  "SQL",
+  "PLpgSQL",
+  "HTML",
+];
+
+const langAliases = {
+  python: "Python",
+  py: "Python",
+  typescript: "TypeScript",
+  ts: "TypeScript",
+  javascript: "JavaScript",
+  js: "JavaScript",
+  ruby: "Ruby",
+  rb: "Ruby",
+  perl: "Perl",
+  pl: "Perl",
+  php: "PHP",
+  lua: "Lua",
+  r: "R",
+  scheme: "Scheme",
+  scm: "Scheme",
+  sql: "SQL",
+  sqlite: "SQLite",
+  postgres: "Postgres",
+  postgresql: "Postgres",
+  plpgsql: "PLpgSQL",
+  html: "HTML",
+  clojurescript: "ClojureScript",
+  clojure: "ClojureScript",
+  cljs: "ClojureScript",
+  json: "JSON",
+  yaml: "YAML",
+  yml: "YAML",
+  text: "Text",
+  txt: "Text",
+  plain: "Text",
+  markdown: "Markdown",
+  md: "Markdown",
+};
+
+const runGist = async (gistId, options = {}) => {
+  const { file, lang } = options;
+  const errEl = document.querySelector(".error");
+
   const response = await fetch(`https://api.github.com/gists/${gistId}`);
   const data = await response.json();
 
   if (!response.ok) {
     console.error(data);
-    const errEl = document.querySelector(".error");
     errEl.textContent = data.message;
     return;
   }
 
-  for (const file of Object.values(data.files)) {
-    if (languageRunners[file.language] && file.language === "Python") {
-      const requirements = Object.values(data.files).find(
-        (file) => file.filename === "requirements.txt",
-      )?.content;
-      languageRunners[file.language](file.content, requirements);
-      return;
-    } else if (languageRunners[file.language]) {
-      languageRunners[file.language](file.content);
+  let targetFile;
+  if (file) {
+    targetFile = Object.values(data.files).find((f) => f.filename === file);
+    if (!targetFile) {
+      errEl.textContent = `File "${file}" not found in gist`;
       return;
     }
+  } else {
+    const sortedFiles = Object.values(data.files).sort((a, b) => {
+      const aPri = languagePriority.indexOf(a.language);
+      const bPri = languagePriority.indexOf(b.language);
+      return (aPri === -1 ? Infinity : aPri) - (bPri === -1 ? Infinity : bPri);
+    });
+    targetFile = sortedFiles.find((f) => languageRunners[f.language]);
   }
 
-  const errEl = document.querySelector(".error");
-  errEl.textContent = "No supported runanble files found in gist";
+  if (!targetFile) {
+    errEl.textContent = "No supported runnable files found in gist";
+    return;
+  }
+
+  const runnerKey = lang ? langAliases[lang.toLowerCase()] : targetFile.language;
+  const runner = languageRunners[runnerKey];
+
+  if (!runner) {
+    errEl.textContent = `Unsupported language: ${lang}`;
+    return;
+  }
+
+  if (runnerKey === "Python") {
+    const requirements = Object.values(data.files).find(
+      (f) => f.filename === "requirements.txt",
+    )?.content;
+    runner(targetFile.content, requirements);
+  } else {
+    runner(targetFile.content);
+  }
 };
 
-const updateRunHref = (id) => {
+const extractGistId = (input) => {
+  const match = input.match(/([a-f0-9]{32})/i);
+  return match ? match[1] : input.trim();
+};
+
+const updateRunHref = (input) => {
+  const id = extractGistId(input);
   const hrefNoParams = window.location.href.split("?")[0];
   const href = `${hrefNoParams}?id=${id}`;
   const runUrl = document.querySelector(".run-url");
@@ -168,7 +669,10 @@ const main = () => {
   const params = new URLSearchParams(window.location.search);
 
   if (params.get("id")) {
-    runGist(params.get("id"));
+    runGist(extractGistId(params.get("id")), {
+      file: params.get("file"),
+      lang: params.get("lang"),
+    });
   }
 
   const gistIdInput = document.querySelector('[name="gist-id"]');
@@ -179,7 +683,7 @@ const main = () => {
 
   document.querySelector("form").addEventListener("submit", (event) => {
     event.preventDefault();
-    runGist(event.target.elements["gist-id"].value);
+    runGist(extractGistId(event.target.elements["gist-id"].value));
   });
 };
 
